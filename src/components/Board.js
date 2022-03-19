314b8a445576d79d42df8225a4656626df23ba43
lizzy wang <yuchen.w@berkeley.edu>
2022-03-18 21:10:02 -0700

    hw4

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,61 @@
 import React from "react";
+import { useState } from "react";
 import './styles/Board.css';
 import Square from "./Square";
 
 function Board() {
 
-    const status = 'Next player: X';
+    var status = 'Next player: X'; // displays text
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [next, setNext] = useState(true);
+
+    function handleClick(square) { // return nothing if won or on existing piece, else place piece
+      const current = board.slice();
+      if (hasWon(current) || current[square]) {
+        return;
+      }
+        current[square] = next ? "X":"O";
+        setBoard(current);
+        next = setNext(!next);
+    }
+
+    function hasWon(board) { // returns player matching winning tiles
+      const winningBoard = 
+        [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6]
+      ]
+      for (let i = 0; i < winningBoard.length; i++) {
+        const [a, b, c] = winningBoard[i];
+        if (board[a] && (board[a] === board[b]) && (board[a] === board[c])) {
+          return board[a];
+        }
+      }
+      return null;  
+    }
+
+    const winner = hasWon(board);
+
+    if (winner) { // displays text
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (next ? "X":"O");
+    }
 
     function renderSquare(i) {
-        return <Square />;
+      return (
+      <Square 
+      value = {board[i]} 
+      onClick = {() => handleClick(i)}/>
+      );
     }
+    
 
     return (  
         <div>
